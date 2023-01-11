1d39a23477e1e3766cd6616646242553edb12722
savaliyarohit <savaliyarohit333@gmail.com>
2023-01-11 16:09:08 +0000

    Dowhileloop

---
+++ Even2.java
@@ -0,0 +1,21 @@
+package Dowhileloop;
+
+public class Even2 {
+    public static void main(String[] args) {
+        int a = 2;{
+            do {
+                if (a % 2 == 0)
+                    System.out.println(a);
+                    a++;
+            }  while (a <= 20) ;
+
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Table3.java
@@ -0,0 +1,14 @@
+package Dowhileloop;
+
+public class Table3 {
+    public static void main(String[] args) {
+       int a=15;
+       int b=1;{
+        do {
+            System.out.println(a + "*" + b + "=" + a * b++);
+        }while (b<=10);
+
+
+        }
+    }
+}
